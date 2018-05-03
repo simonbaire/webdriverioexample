9f0d57910007d539f848d10830f106b32748d263
Simon Baire <simon.baire@mendeley.com>
2018-05-03 14:58:06 +0100

    add comments

--- test/config/conf.js
+++ test/config/conf.js
@@ -1,13 +1,12 @@
 const defaultTimeoutInterval = process.env.DEBUG ? (60 * 60 * 500) : 90000;
 
 exports.config = {
-
+   // Path to execute tests
     specs: [
         './test/features/*.feature',
     ],
-
+  // Default browser to use
     capabilities: [
-
           {
               browserName: 'chrome'
           },
@@ -16,6 +15,7 @@ exports.config = {
     // Test Configurations
     // ===================
     // Set webdriverIO commands to be synchronous
+    // define all options for webdriverIO instance
     sync: true,
     screenshotPath: './test/reports/errorShots/',
     baseUrl: 'http://',
@@ -35,7 +35,7 @@ exports.config = {
         format: ['pretty'], // report format
         timeout: defaultTimeoutInterval,    // timeout for step definitions
     },
-
+   // user chai for  test assertions
     before: function() {
       const chai    = require('chai');
       global.expect = chai.expect;
