a435704b43c7ab0f0e5e12c12fee6b11c9b3afa8
Simon Baire <simon.baire@mendeley.com>
2018-05-03 16:55:57 +0100

    remove unused code

--- test/config/conf.js
+++ test/config/conf.js
@@ -31,7 +31,6 @@ exports.config = {
     cucumberOpts: {
         require: ['./test/stepDefinitions/given.js', './test/stepDefinitions/when.js', './test/stepDefinitions/then.js'],
         backtrace: true,
-        compiler: ['js:babel-core/register'],
         format: ['pretty'], // report format
         timeout: defaultTimeoutInterval,    // timeout for step definitions
     },
